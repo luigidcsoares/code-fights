ef202baa74dc88343dca1ae1d05407c66b71cc0e
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-02-10 13:21:48 -0200

    FirstNotRepeatingCharacter :: Elixir

---
+++ interview-practice/data-structures/arrays/first_not_repeating_character.ex
@@ -0,0 +1,21 @@
+def frequencies(coll) do
+  Enum.reduce(
+    coll,
+    %{},
+    fn x, acc ->
+      Map.update(acc, x, 1, &(&1 + 1))
+    end
+  )
+end
+
+def first_not_repeating_character(s) do
+  graphemes = String.graphemes(s)
+  freq = frequencies(graphemes)
+
+  graphemes
+  |> Enum.drop_while(fn x -> freq[x] > 1 end)
+  |> (fn
+        [] -> "_"
+        [head | _] -> head
+      end).()
+end
